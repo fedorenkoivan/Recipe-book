e9175f38fec5de88ca925ca92cf99293416bb6e9
fedorenkoivan <ivan.r.fedorenko@gmail.com>
2025-05-16 16:23:37 +0300

    add basic config for api usage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import recipesRouter from './routes/recipes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/recipes', recipesRouter);
